bec29653812ce4ec313e66138339b3195a84852c
Muxing Chen <ike.chan@outlook.com>
2025-04-15 04:09:21 -0400

    Add Bar component for rendering bar charts with dynamic data

---
+++ src/lib/components/Bar.svelte
@@ -0,0 +1,17 @@
+<script>
+	let { data, xProperty, yProperty, xBandScale, yLinearScale, truncation = 0 } = $props();
+</script>
+
+<g class="bar-chart">
+	{#each data as bar, i}
+		<rect
+			role="graphics-symbol"
+			x={xBandScale(bar[xProperty])}
+			y={yLinearScale(bar[yProperty])}
+			height={yLinearScale(0) - yLinearScale(bar[yProperty] - truncation)}
+			width={xBandScale.bandwidth()}
+			fill={"#d9d9d9"}
+			opacity={0.8}
+		/>
+	{/each}
+</g>
