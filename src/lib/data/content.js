8582574a2557996990e9b39c2382e9e322dfb79d
Muxing Chen <ike.chan@outlook.com>
2025-04-15 04:51:52 -0400

    Revise content

--- src/lib/data/content.js
+++ src/lib/data/content.js
@@ -2,12 +2,12 @@ export const content = [
 	{
 		subtitle: null,
 		paragraph:
-			"In the past decade, the US has suffered an unprecedented economic toll from hurricanes. Six of the most costly hurricane seasons have occurred since 2005, with damages skyrocketing into hundreds of billions of dollars. But what's driving these astronomical costs? Are hurricanes becoming more frequent, more intense, or are we simply putting more in harm's way? Recent data from the National Oceanic and Atmospheric Administration reveals a complex story. "
+			"In the past decade, the US has suffered an unprecedented economic toll from hurricanes. Six of the <a href='https://en.wikipedia.org/wiki/List_of_costliest_Atlantic_hurricanes' target='_blank'>most costly hurricane seasons</a> have occurred since 2005, with damages skyrocketing into hundreds of billions of dollars. But what's driving these astronomical costs? Are hurricanes becoming more frequent, more intense, or are we simply putting more in harm's way? Recent data from the <a href='https://www.ncei.noaa.gov/access/billions/time-series' target='_blank'>National Oceanic and Atmospheric Administration</a> reveals a complex story."
 	},
 	{
 		subtitle: "Twice the Storms, Eleven Times the Cost",
 		paragraph:
-			"Since the 1980s, hurricanes have caused eleven times more economic loss with just doubling the frequency. The astronomical costs are primarily driven by a handful of destructive hurricanes. For example, in 2017, hurricanes Harvey, Maria, and Irma contributed around 341.9 billion to the total cost of 384.61 billion, accounting for nearly 90% of that season's staggering $384.61 toll."
+			"Since the 1980s, hurricanes have caused eleven times more economic loss with just doubling the frequency. The astronomical costs are primarily driven by a handful of destructive hurricanes. For example, in 2017, hurricanes Harvey, Maria, and Irma contributed around 341.9 billion in damages, accounting for nearly 90% of that season's staggering $384.61 toll."
 	},
 	{
 		subtitle: "More coastal population, more cost",
@@ -15,13 +15,13 @@ export const content = [
 			"People have increasingly flocked to hurricane-threaten coastal counties, creating denser concentrations of people, properties, and businesses along the path of hurricanes. The population along the Gulf Coast and the Southeastern shoreline—the most vulnerable regions to hurricanes—has increased from 17.8 million in 1990 to 27.85 million in 2022, a 36% growth. The migration and property development show no signs of slowing down."
 	},
 	{
-		subtitle: "Stronger wind and water, a duo of culprits",
+		subtitle: "Stronger storms, heavier rains",
 		paragraph:
-			"According to Christopher Flavelle, who covers climate at The New York Times, 'Stronger storms plus more development in coastal areas equals more damage.' 'Stronger' extends beyond just wind speeds. In a CNN interview with Philip Klotzbach, a senior research scientist in the atmospheric science department at Colorado State University, he pointed out that storms tend to intensify more quickly, move slower, and dump substantially more rain—a devastating combination that maximizes destruction."
+			"According to Christopher Flavelle, who covers climate at <a href='https://www.nytimes.com/2024/04/15/climate/hurricane-costs-climate-change.html' target='_blank'>The New York Times</a>, 'Stronger storms plus more development in coastal areas equals more damage.' 'Stronger' extends beyond just wind speeds. In a <a href='https://www.cnn.com/2024/04/15/climate/hurricane-costs-climate-change/index.html' target='_blank'>CNN</a> interview with Philip Klotzbach, a senior research scientist in the atmospheric science department at Colorado State University, he pointed out that storms tend to intensify more quickly, move slower, and dump substantially more rain—a devastating combination that maximizes destruction."
 	},
 	{
 		subtitle: "Brace for a Stormier Future",
 		paragraph:
-			"With millions of people and stuff settled in hurricane-prone regions, and they can't be transferred in the short term, experts proposed solutions like making buildings more wind-resistant and upgrading infrastructures that block storm surges and quickly pump away rainwater. However, it is difficult to predict the effectiveness and benefits of these measures. After all, these measures require greater investment, which means more expensive assets are placed in front of the hurricane."
+			"With millions of people and valuable assets already settled in hurricane-prone regions—and unlikely to relocate in the short term—experts proposed solutions like making buildings more wind-resistant and upgrading infrastructures that block storm surges and quickly pump away rainwater. However, it is difficult to predict the effectiveness and benefits of these measures. After all, these measures require greater investment, which means more expensive assets are placed in front of the hurricane."
 	}
 ];
