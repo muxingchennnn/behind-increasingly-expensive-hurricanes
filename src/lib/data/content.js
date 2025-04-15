2e749991e5134eb0d9a59b890df84dfb554b4b60
Muxing Chen <ike.chan@outlook.com>
2025-04-15 04:07:39 -0400

    Add data

---
+++ src/lib/data/content.js
@@ -0,0 +1,27 @@
+export const content = [
+	{
+		subtitle: null,
+		paragraph:
+			"In the past decade, the US has suffered an unprecedented economic toll from hurricanes. Six of the most costly hurricane seasons have occurred since 2005, with damages skyrocketing into hundreds of billions of dollars. But what's driving these astronomical costs? Are hurricanes becoming more frequent, more intense, or are we simply putting more in harm's way? Recent data from the National Oceanic and Atmospheric Administration reveals a complex story. "
+	},
+	{
+		subtitle: "Twice the Storms, Eleven Times the Cost",
+		paragraph:
+			"Since the 1980s, hurricanes have caused eleven times more economic loss with just doubling the frequency. The astronomical costs are primarily driven by a handful of destructive hurricanes. For example, in 2017, hurricanes Harvey, Maria, and Irma contributed around 341.9 billion to the total cost of 384.61 billion, accounting for nearly 90% of that season's staggering $384.61 toll."
+	},
+	{
+		subtitle: "More coastal population, more cost",
+		paragraph:
+			"People have increasingly flocked to hurricane-threaten coastal counties, creating denser concentrations of people, properties, and businesses along the path of hurricanes. The population along the Gulf Coast and the Southeastern shoreline—the most vulnerable regions to hurricanes—has increased from 17.8 million in 1990 to 27.85 million in 2022, a 36% growth. The migration and property development show no signs of slowing down."
+	},
+	{
+		subtitle: "Stronger wind and water, a duo of culprits",
+		paragraph:
+			"According to Christopher Flavelle, who covers climate at The New York Times, 'Stronger storms plus more development in coastal areas equals more damage.' 'Stronger' extends beyond just wind speeds. In a CNN interview with Philip Klotzbach, a senior research scientist in the atmospheric science department at Colorado State University, he pointed out that storms tend to intensify more quickly, move slower, and dump substantially more rain—a devastating combination that maximizes destruction."
+	},
+	{
+		subtitle: "Brace for a Stormier Future",
+		paragraph:
+			"With millions of people and stuff settled in hurricane-prone regions, and they can't be transferred in the short term, experts proposed solutions like making buildings more wind-resistant and upgrading infrastructures that block storm surges and quickly pump away rainwater. However, it is difficult to predict the effectiveness and benefits of these measures. After all, these measures require greater investment, which means more expensive assets are placed in front of the hurricane."
+	}
+];
---
+++ src/lib/data/waterfall.js
@@ -0,0 +1,82 @@
+export const waterFallData = [
+	{
+		year: 2017,
+		item: "Total",
+		cost: 384.6
+	},
+	{
+		year: 2017,
+		item: "Harvey",
+		cost: -161.3
+	},
+	{
+		year: 2017,
+		item: "Maria",
+		cost: -116.1
+	},
+	{
+		year: 2017,
+		item: "Irma",
+		cost: -64.5
+	},
+	{
+		year: 2017,
+		item: "Others",
+		cost: -42.7
+	},
+	{
+		year: 2005,
+		item: "Total",
+		cost: 282.1
+	},
+	{
+		year: 2005,
+		item: "Katrina",
+		cost: -202.5
+	},
+	{
+		year: 2005,
+		item: "Wilma",
+		cost: -30.4
+	},
+	{
+		year: 2005,
+		item: "Rita",
+		cost: -29.8
+	},
+	{
+		year: 2005,
+		item: "Dennis",
+		cost: -4.1
+	},
+	{
+		year: 2005,
+		item: "Others",
+		cost: -15.3
+	},
+	{
+		year: 2024,
+		item: "Total",
+		cost: 129.5
+	},
+	{
+		year: 2024,
+		item: "Helene",
+		cost: -79.5
+	},
+	{
+		year: 2024,
+		item: "Milton",
+		cost: -34.6
+	},
+	{
+		year: 2024,
+		item: "Beryl",
+		cost: -7.4
+	},
+	{
+		year: 2024,
+		item: "Others",
+		cost: -17.4
+	}
+];
